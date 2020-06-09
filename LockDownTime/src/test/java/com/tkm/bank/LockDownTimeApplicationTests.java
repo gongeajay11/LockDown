9ccb59b26c3669a75fb111770acc6521684c8b58
ajay <ajay@AJAY-PC>
2020-06-09 14:57:56 +0530

    first commit

---
+++ LockDownTime/src/test/java/com/tkm/bank/LockDownTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tkm.bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LockDownTimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
